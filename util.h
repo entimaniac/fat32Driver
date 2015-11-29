9e0eac0b084cb7886698c23ec40b119e84fe2853
entimaniac <entimaniac91@yahoo.com>
2015-11-29 17:43:43 -0500

    added functionality for checking correct argument counts
    no more seg faults on bad argument usage

--- util.h
+++ util.h
@@ -13,10 +13,11 @@ static const int FREE_DIR = 0xE5; //229
 static const int ZERO_DIR = 0;
 static const int EOC = 0x0FFFFFF8;
 static const int EOC2 = 0x0FFFFFFF;
-typedef enum {ATTR_READ_ONLY,
+typedef enum {
+	ATTR_READ_ONLY,
 	ATTR_HIDDEN,
-        ATTR_SYSTEM,
-        ATTR_VOLUME_ID,
+	ATTR_SYSTEM,
+	ATTR_VOLUME_ID,
 	ATTR_DIRECTORY,
 	ATTR_ARCHIVE,
 	ATTR_LONG_NAME
@@ -42,26 +43,28 @@ struct directory{
 	ATTR Attribute;	
 };
 
+/******************************************************************************/
+/*                      Function Prototypes */
+/******************************************************************************/
 
-unsigned int getValueFromBootSector(unsigned char*, int, int);
-void getValueFromDirectorySector(unsigned char*, unsigned char*, int, int);
-void getFileName(unsigned char*,unsigned char*,int);
-struct directory getDirectoryInformation(unsigned char*, int);
-unsigned int currentClusterNumber(MODE,unsigned int);
-void presentWorkingDirectory(MODE,char*);
-void getCluster(struct directory*,unsigned char*,unsigned int,unsigned int,
-			unsigned int, unsigned int, unsigned int);
+unsigned int getValueFromBootSector( unsigned char*, int, int );
+void getValueFromDirectorySector( unsigned char*, unsigned char*, int, int );
+void getFileName( unsigned char*, unsigned char*, int );
+struct directory getDirectoryInformation( unsigned char*, int );
+unsigned int currentClusterNumber( MODE, unsigned int );
+void presentWorkingDirectory( MODE, char* );
+void getCluster( struct directory*, unsigned char*, unsigned int, unsigned int,
+	unsigned int, unsigned int, unsigned int);
 
-void removeTrailingNewline(char*);
+void removeTrailingNewline( char* );
 
-unsigned int parseInput(struct directory*,unsigned char*,unsigned int,unsigned int,
-			unsigned int,unsigned int,char*,char*);
-int isCommand(struct directory*,unsigned char*,unsigned int,unsigned int,
-			unsigned int, unsigned int, char*,char*);
-int isFile(struct directory*,char*);
-int isDir(struct directory*,char*);
-
-
+unsigned int parseInput( struct directory*, unsigned char*, unsigned int, 
+	unsigned int, unsigned int, unsigned int, char*, char* );
+int isCommand( struct directory*, unsigned char*, unsigned int, unsigned int,
+	unsigned int, unsigned int, char*, char* );
+int isFile( struct directory*, char* );
+int isDir( struct directory*, char* );
+int checkArgumentCount ( int argNum, int numReq );
 
 
 
